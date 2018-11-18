259150f2c3d0302b7d92ca879d1e016a77770397
Alejandro Navarro <alexnav964@gmail.com>
2018-11-18 15:17:34 -0600

    Add files via upload

--- SDL_tutorial/main.cpp
+++ SDL_tutorial/main.cpp
@@ -1,102 +1,80 @@
 /*
- * main.cpp
- * This file is the one that
- *
- *
- */
+Author: Alejandro Navarro
+Title: Platform
+Description: header file of the class platform
+Date Created: 11-2-2018
+Date Last Modified: 11-18-2018
+*/
 
 #include <iostream>
-
+#include <SDL.h>
 #include "SDL_Plotter.h"
-#include "Characters.h"
-#include "Pow.h"
-#include "Color.h"
-#include "Draw.h"
-#include "Objects.h"
 #include "platform.h"
-
 
 using namespace std;
 
+int main(int argc, char ** argv)
+{
+
+    int rows = 800; //multiple of 10
+    int cols = 1120; //multiple of 14
+
+    SDL_Plotter g(rows, cols);
+    platform plat(g, rows, cols);
+
+    SDL_Surface* image = g.loadImage("Rick.bmp");
+    cout << image->w << "x" << image->h << endl;
 
 
-int main(int argc, char ** argv){
-    
-    const int SCALE = 5;
-    
-    //create the window
-    SDL_Plotter g(800,1120);
-    
-    //draw background and platform
-    drawBackground(g);
-    platform( g, 800, 1120);
-    
-    
-    
-    Color c0(51, 92, 24),
-    c1(63, 114, 28),
-    c2(101, 144, 69),
-    c3(40, 75, 15),
-    c4(168, 229, 255),
-    c5(BLACK),
-    c6(141, 184, 212);
-    
-    Color pickleArray[18][20] = {
-        {Color(c5), Color(c2), Color(c1), Color(c0), Color(c3), Color(c5)},
-        {Color(c2), Color(c0), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c4), Color(c4), Color(c4), Color(c6), Color(c0)},
-        {Color(c2), Color(c5), Color(c0), Color(c0), Color(c5), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c1), Color(c3)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c1), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c1), Color(c0)},
-        {Color(c2), Color(c0), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c0), Color(c3)},
-        {Color(c0), Color(c1), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c0), Color(c0), Color(c0)},
-        {Color(c2), Color(c1), Color(c0), Color(c1), Color(c0), Color(c3)},
-        {Color(c2), Color(c1), Color(c0), Color(c1), Color(c0), Color(c3)},
-        {Color(c2), Color(c1), Color(c1), Color(c3), Color(c3), Color(c3)},
-        {Color(c5), Color(c2), Color(c3), Color(c0), Color(c3), Color(c5)} };
-    
-    Objects pickle( 18, 6);
-    
-    
-    
-    //SDL_Plotter g, int scale, int xPosition, int yPosition, int array height,
-    //int array width, and the array
-    
-    drawArray( g, SCALE, 100, 100, 18, 6, pickleArray);
-    
-    
-    while (!g.getQuit()){
-        
-        
-        
-        g.update();
-        if(g.kbhit()){
-            g.getKey();
+
+
+
+    int x = 50, y = 50;
+    g.displayImage(image, x, y);
+
+    while(g.getKey() != 'Q')
+    {
+
+      if(g.kbhit())
+      {
+        if(g.getKey() == 'W')
+        {
+          if(!plat.touchEdge(x, y - 82))
+          {
+            y -= 2;
+          }
         }
+        else if(g.getKey() == 'S')
+        {
+          if(!plat.touchEdge(x, y + 82))
+          {
+            y += 2;
+          }
+        }
+        else if(g.getKey() == 'D')
+        {
+          if(!plat.touchEdge(x + 29, y))
+          {
+            x += 2;
+          }
+        }
+        else if(g.getKey() == 'A')
+        {
+          if(!plat.touchEdge(x - 29, y))
+          {
+            x -= 2;
+          }
+        }
+
+        g.displayImage(image, x, y);
+
+      }
+
+    	g.update();
+
     }
+
+
+  return 0;
+
 }
-
-
-/*
- x = rand()%g.getCol();
- y = rand()%(g.getRow()/16)+750;
- 
- 
- 
- Color object;
- x = rand()%g.getCol();
- y = rand()%g.getRow();
- 
- for(xd = 0; xd < 10 && x + xd < g.getCol(); xd++ ){
-    for(yd = 0; yd < 10 && y + yd < g.getRow(); yd++ ){
-        //g.plotPixel( x + xd, y + yd, 255, 255, 255);
-        g.plotPixel( x + xd, y + yd, WHITE);
-    }
- }
- */
