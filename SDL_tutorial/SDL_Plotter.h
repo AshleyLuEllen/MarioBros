f1b32d661164e935e9f4f30defba9338ac3919f7
Alejandro Navarro <alexnav964@gmail.com>
2018-11-18 15:02:40 -0600

    Update SDL_Plotter.h

--- SDL_tutorial/SDL_Plotter.h
+++ SDL_tutorial/SDL_Plotter.h
@@ -1,8 +1,10 @@
 /*
  * SDL_Plotter.h
  * Version 2.1
- *  1/26/2018
- *  Dr. Booth
+ *  Author: Dr. Booth
+ *  Date Created: 1/26/2018
+ *  Modified by: Ashley Couch and Alejandro Navarro
+ *  Date Last Modified: 11/18/2018
  */
 
 #ifndef SDL_PLOTTER_H_
@@ -123,6 +125,9 @@ public:
     
     void getMouseLocation(int &x, int &y);
     
+    SDL_Surface* loadImage(string imagename);
+    
+    void displayImage(SDL_Surface* image, int x, int y);
 };
 
 #endif /* SDL_PLOTTER_H_ */
