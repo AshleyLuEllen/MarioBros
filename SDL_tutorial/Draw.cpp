c01440285c4a55917123a04cfeb0682572191cac
AshleyLuEllen <ashleylu_couch1@baylor.edu>
2018-11-19 13:40:09 -0600

    Redid draw finctions, added a function to draw flipped.

--- SDL_tutorial/Draw.cpp
+++ SDL_tutorial/Draw.cpp
@@ -7,6 +7,8 @@
 
 #include <stdio.h>
 #include "Draw.h"
+#include "Color.h"
+#include "Objects.h"
 
 
 void drawArray(SDL_Plotter & g, int s, int x_, int y_, int h, int w, Color array[][20]){
@@ -23,6 +25,26 @@ void drawArray(SDL_Plotter & g, int s, int x_, int y_, int h, int w, Color array
             for(int xd = 0; xd < SCALE && x + col * SCALE + xd < g.getCol(); xd++){
                 for(int yd = 0; yd < SCALE && y + row * SCALE + yd < g.getRow(); yd++){
                     g.plotPixel( x + col * SCALE + xd, y + row * SCALE + yd, array[row][col]);
+                }
+            }
+        }
+    }
+}
+
+void drawArrayFlip(SDL_Plotter & g, int s, int x_, int y_, int h, int w, Color array[][20]){
+    int height = h;
+    int width = w;
+    
+    int x = x_;
+    int y = y_;
+    
+    const int SCALE = s;
+    
+    for(int row = height-1; row >= 0; row--){
+        for(int col = width-1; col >= 0; col--){
+            for(int xd = 0; xd < SCALE && x + col * SCALE + xd < g.getCol(); xd++){
+                for(int yd = 0; yd < SCALE && y + row * SCALE + yd < g.getRow(); yd++){
+                    g.plotPixel( x + col * SCALE + xd, y + row * SCALE + yd, array[height - row -1][col]);
                 }
             }
         }
@@ -54,3 +76,20 @@ void drawBackground(SDL_Plotter & g){
     }
     g.update();
 }
+
+void drawPickle(SDL_Plotter & g, int s, int x_, int y_, int h, int w){
+    drawArray(g, s, x_, y_, 18 , 6, pickleArray);
+}
+
+void drawPickleFlip(SDL_Plotter & g, int s, int x_, int y_, int h, int w){
+    drawArrayFlip(g, s, x_, y_, 18 , 6, pickleArray);
+}
+
+
+
+
+
+
+
+
+
--- SDL_tutorial/Draw.h
+++ SDL_tutorial/Draw.h
@@ -12,23 +12,14 @@
 #include "Color.h"
 
 
-class Draw{
-private:
-    
-    
-public:
-    
-
-    
-
-};
-
-void drawArray();
 void drawArray(SDL_Plotter & g, int s, int x_, int y_, int h, int w, Color array[][20]);
+
+void drawArrayFlip(SDL_Plotter & g, int s, int x_, int y_, int h, int w, Color array[][20]);
 
 void drawBackground(SDL_Plotter & g);
 
-
+void drawPickle(SDL_Plotter & g, int s, int x_, int y_, int h, int w);
+void drawPickleFlip(SDL_Plotter & g, int s, int x_, int y_, int h, int w);
 
 
 #endif /* Draw_h */
