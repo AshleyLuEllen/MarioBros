908cc5daa87621fd35f96f2519ecae838658a677
AshleyLuEllen <ashleylu_couch1@baylor.edu>
2018-11-29 13:41:32 -0600

    Added function block comments

--- Morty Bros/Tool.h
+++ Morty Bros/Tool.h
@@ -23,12 +23,36 @@ class Timer
   bool complete;
 
   public:
+    /*
+     description:   This function resets the enemy to original start
+     return:        void
+     precondition:  Plotter and platform are defined
+     postcondition: creates a timer
+     */
   Timer(int ms);
 
+    /*
+     description:   restes the timer complete is false
+     return:        void
+     precondition:  Plotter and platform are defined
+     postcondition: restes the timer complete is false
+     */
   void reset();
 
+    /*
+     description:   returns the value of complete
+     return:        bool
+     precondition:  complete is defined
+     postcondition: complete is returned
+     */
   bool isComplete();
 
+    /*
+     description:   This function resets the enemy to original start
+     return:        void
+     precondition:  ms is defined
+     postcondition: the enemy is reset and to original conditions
+     */
   void setTimer(int ms);
 
 };
